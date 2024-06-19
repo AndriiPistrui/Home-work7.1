8b5abc3a74f4e5880ee58a227848176801759830
AndriiPistrui <andrejpistruj06@gmail.com>
2024-06-19 15:18:16 +0300

    Add files via upload

---
+++ Home-work 7.1/main.py
@@ -0,0 +1,6 @@
+def say_hi(name, age):
+    return f"Hi. My name is {name} and I'm {age} years old"
+
+assert say_hi("Andrei", 25) == "Hi. My name is Andrei and I'm 25 years old", 'Test1'
+assert say_hi("Kostya", 34) == "Hi. My name is Kostya and I'm 34 years old", 'Test2'
+print('ОК')
